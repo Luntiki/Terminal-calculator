eed5b6df13c74943c37766acb8787ed15d3b1bbb
Luntiki <su6639490@gmail.com>
2025-05-30 15:17:14 +0300

    error corrction

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from math_operations import amount, difference, composition, division, degre, quotient, remainder
 
 def two_numbers():
-    first_number=input('Please write the first number:' )
-    second_number=input('Please write the second number:' )
-
+    first_number=input('Please write the first number: ')
+    second_number=input('Please write the second number: ')
     try:
         if list(first_number).count('.') > 0 or list(second_number).count('.') > 0: 
             first_number = float(first_number)
@@ -14,22 +13,26 @@ def two_numbers():
     except ValueError:
         return('Error. Please write an intenger or decimal number')
 
-    print('Possible actions: AMOUNT,  DIFFERENCE, COMPOSITION, DIVISION, DEGRE, QUOTIENT, REMAINDER')
-    action=input('Pleace enter the action you want to perform on the numbers: ')
 
-    operations={
-        'amount': amount,
-        'difference': difference,
-        'composiotion': composition,
-        'division': division,
-        'degre': degre,
-        'quotient': quotient,
-        'remainder': remainder,
-    }
-    action_lower=action.lower() 
 
-    if action_lower in operations:
-        return(operations[action_lower](first_number, second_number))
-    else:
-        print('The operatin was not found. Check the correctness of the introduction')
-print(two_numbers())
+    def choosing_an_action(first_number, second_number):
+        print('Possible actions: AMOUNT,  DIFFERENCE, COMPOSITION, DIVISION, DEGRE, QUOTIENT, REMAINDER')
+        action=input('Please enter the action you want to perform on the numbers: ')
+        operations={
+            'amount': amount,
+            'difference': difference,
+            'composiotion': composition,
+            'division': division,
+            'degre': degre,
+            'quotient': quotient,
+            'remainder': remainder,
+        }
+        action_lower=action.lower() 
+        try:
+            return operations[action_lower](first_number, second_number)
+        except KeyError:
+            print('The operatin was not found. Check the correctness of the introduction')
+            choosing_an_action(first_number, second_number)
+
+    print(choosing_an_action(first_number, second_number))
+two_numbers()
