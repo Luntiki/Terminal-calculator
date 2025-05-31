02c1f85d258ee2c0fdea938377280860c1984149
Luntiki <su6639490@gmail.com>
2025-05-31 12:07:44 +0300

    eupdate main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from functions import one_number, two_numbers, set_of_numbers
 
-while True:
-    quantity=int(input("Enter the number of numbers: "))
-    if quantity == 1:
-        print(one_number())
-    elif quantity == 2: 
-        print(two_numbers())
-    elif quantity>2:
-        print(set_of_numbers())
+def main():
+    print('What do you want to spend? operations for a single number (ofs) or operations for two numbers (oft) or operations for multiple numbers(ofm)')
+    main_action=input("Please write your action in a small format: ")
+    if main_action == "ofs":
+        return one_number()
+    elif main_action == "oft":
+        return two_numbers()
+    elif main_action == "ofm":
+        return set_of_numbers()
     else:
-        print('Please enter the correct value')
+        print("Error. Please enter the correct value")
+        return main()
 
+
+print(main())
